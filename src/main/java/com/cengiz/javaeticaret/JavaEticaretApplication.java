a18886ddcc767417040eb50cfb5f4e4e5664dc7e
Cengiz Ozdemir <cengizank18@gmail.com>
2024-11-08 13:04:42 +0300

    first commit

---
+++ src/main/java/com/cengiz/javaeticaret/JavaEticaretApplication.java
@@ -0,0 +1,13 @@
+package com.cengiz.javaeticaret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEticaretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEticaretApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cengiz/javaeticaret/JavaEticaretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengiz.javaeticaret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEticaretApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
